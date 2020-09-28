0ad8380e11261c3c05a252ea92b9597ca03c18f4
Cody Pellom <cpellom@APT01-FVFC80ADL413.local>
2020-09-28 11:20:17 -0400

    monday init

---
+++ js/base.js
@@ -0,0 +1,9 @@
+function navBarLandingPageBgHeight() {
+    debugger;;;
+    var navbar = document.getElementById("navWrapper");
+    var navHeight = navbar.offsetHeight;
+    var landingBg = document.getElementById("preloginLanding");
+    console.log(navbar);
+}
+
+navBarLandingPageBgHeight();

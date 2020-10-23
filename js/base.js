ce4660aa0d4e16aec052f241ecf6e04d9ff77af4
Cody Pellom <cpellom@APT01-FVFC80ADL413.local>
2020-10-23 18:26:33 -0400

    added scroll sticky nav

--- js/base.js
+++ js/base.js
@@ -1,8 +1,31 @@
-function navBarLandingPageBgHeight() {
-    var navbar = document.getElementById("navWrapper");
-    var navHeight = navbar.offsetHeight;
-    var landingBg = document.getElementById("preloginLanding");
-    console.log(navbar);
-}
+function init() {
+    var me = this;
+    me.navbarFadeInOnScroll();
+};
 
-navBarLandingPageBgHeight();
+function navbarFadeInOnScroll() {
+    var navbar = document.getElementById('navBar');
+    var navLogo = document.getElementById('brandLogo');
+
+    window.onscroll = function() {
+        var scrollPosition = window.scrollY;
+        if (scrollPosition >= 1) {
+            navbar.style.backgroundColor = '#D1C4E9';
+            navLogo.style.color = '#fff';
+        } else {
+            navbar.style.backgroundColor = 'transparent';
+            navLogo.style.color = '#D1C4E9';
+            navbar.style.boxShadow = 'unset';
+            navbar.style.webkitBoxShadow = 'unset';
+        }
+    };
+    // window.addEventListener(scrollPosition, (event) => {
+    //     if (scrollPosition > 5) {
+    //         navbar.style.backgroundColor = "#D1C4E9";
+    //     };
+    // });
+    
+};
+
+init();
+

4502eb3687b2acefd315b377d1289df70568e782
Cody Pellom <cpellom@APT01-FVFC80ADL413.local>
2020-10-23 19:12:18 -0400

    added widget bar

--- js/base.js
+++ js/base.js
@@ -1,31 +1,35 @@
 function init() {
     var me = this;
     me.navbarFadeInOnScroll();
+    me.widgetsPopout();
 };
 
 function navbarFadeInOnScroll() {
     var navbar = document.getElementById('navBar');
+    var widgets = document.getElementById('widgetSection');
     var navLogo = document.getElementById('brandLogo');
-
+    navbar.style.backgroundColor = 'transparent';
     window.onscroll = function() {
         var scrollPosition = window.scrollY;
         if (scrollPosition >= 1) {
+            navbar.classList.remove('transparent');
             navbar.style.backgroundColor = '#D1C4E9';
+            widgets.style.backgroundColor = '#f06292';
             navLogo.style.color = '#fff';
         } else {
             navbar.style.backgroundColor = 'transparent';
+            widgets.style.backgroundColor = 'transparent';
             navLogo.style.color = '#D1C4E9';
             navbar.style.boxShadow = 'unset';
             navbar.style.webkitBoxShadow = 'unset';
         }
     };
-    // window.addEventListener(scrollPosition, (event) => {
-    //     if (scrollPosition > 5) {
-    //         navbar.style.backgroundColor = "#D1C4E9";
-    //     };
-    // });
-    
 };
+
+function widgetsPopout() {
+    var widgets = document.getElementById('widgetSection');
+    widgets.style.backgroundColor = 'green';
+}
 
 init();
 

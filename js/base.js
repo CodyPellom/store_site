7dd25f56d8e655acad6e57ed0e9fa083111296f0
Cody Pellom <cpellom@APT01-FVFC80ADL413.local>
2020-10-24 17:28:33 -0400

    SEXY NAVBAR LOGIC

--- js/base.js
+++ js/base.js
@@ -1,34 +1,44 @@
 function init() {
     var me = this;
     me.navbarFadeInOnScroll();
-    me.widgetsPopout();
 };
 
 function navbarFadeInOnScroll() {
     var navbar = document.getElementById('navBar');
     var widgets = document.getElementById('widgetSection');
+    var wItems = document.getElementsByClassName('widget-item');
     var navLogo = document.getElementById('brandLogo');
     navbar.style.backgroundColor = 'transparent';
     window.onscroll = function() {
         var scrollPosition = window.scrollY;
-        if (scrollPosition >= 1) {
+        if (scrollPosition > 1) {
             navbar.classList.remove('transparent');
             navbar.style.backgroundColor = '#D1C4E9';
-            widgets.style.backgroundColor = '#f06292';
             navLogo.style.color = '#fff';
-        } else {
+        } if (scrollPosition === 0) {
             navbar.style.backgroundColor = 'transparent';
             widgets.style.backgroundColor = 'transparent';
-            navLogo.style.color = '#D1C4E9';
-            navbar.style.boxShadow = 'unset';
-            navbar.style.webkitBoxShadow = 'unset';
+            for(var i = 0; i < wItems.length; i++) {
+                wItems[i].style.color = 'transparent';
+                }
+
         }
     };
 };
 
 function widgetsPopout() {
     var widgets = document.getElementById('widgetSection');
-    widgets.style.backgroundColor = 'green';
+    var wItems = document.getElementsByClassName('widget-item');
+    var counter = 0;
+    widgets.style.backgroundColor = '#f06292';
+        for(var i = 0; i < wItems.length; i++) {
+            wItems[i].style.color = 'white';
+            }
+    
+    // This makes the text transparent
+    // for(var i = 0; i < wItems.length; i++) {
+    //     wItems[i].style.color = 'transparent';
+    // }
 }
 
 init();
